7ba01fa0f637f0439632b0ce3d49dd93d947d495
Ziggornif <matthieudrouian@gmail.com>
2017-03-01 09:05:32 +0000

    Amélioration de la fonction retournant la charge CPU Mise en place de fonctions fléchées

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 'use strict'
 const q = require('q');
-var os = require('os');
+require('q-foreach')(q);
+const os = require('os');
 const _ = require('lodash');
 const ps = require('current-processes');
 const childProcess = require('child_process')
@@ -13,58 +14,59 @@ exports.uptime = function() {
     return os.uptime();
 }
 
-exports.cpuLoad = function() {
-    var deffered = q.defer();
-    var beforeCpuInfos = getCPUInfo();
-
-    setTimeout(function() {
-        var afterCpuInfos = getCPUInfo();
-
-        var idle = afterCpuInfos.idle - beforeCpuInfos.idle;
-        var total = afterCpuInfos.total - beforeCpuInfos.total;
-        var perc = idle / total;
-
+exports.cpuLoad = () => {
+    let deffered = q.defer();
+    let beforeCpuInfos;
+    getCPUInfo().then(result => {
+        beforeCpuInfos = result;
+        return q.delay(1000);
+    }).then(() =>{
+        return getCPUInfo();
+    }).then(afterCpuInfos =>{
+        let idle = afterCpuInfos.idle - beforeCpuInfos.idle;
+        let total = afterCpuInfos.total - beforeCpuInfos.total;
+        let perc = idle / total;
         deffered.resolve(Math.floor((1 - perc) * 100));
+    });
 
-    }, 1000);
     return deffered.promise;
 }
 
-exports.memoryUsage = function() {
-    var deffered = q.defer();
-    var computeUsage = function(used, total) {
+exports.memoryUsage = () => {
+    let deffered = q.defer();
+    let computeUsage = (used, total) => {
         return Math.round(100 * (used / total));
     };
-    
+
     //Windows platform
     if (process.platform === 'win32') {
         q.all([
             winGetFreeMemory(),
             winGetTotalMemory()
-        ]).then(function(results) {
+        ]).then(results => {
             deffered.resolve(100 - computeUsage(results[0], results[1]));
-        }, function(err) {
+        }, err => {
             deffered.reject(err);
         });
-    //MacOSX platform
+        //MacOSX platform
     } else if (process.platform === "darwin") {
-        childProcess.exec('memory_pressure | grep "System-wide memory free percentage: "', function (err, stdout) {
+        childProcess.exec('memory_pressure | grep "System-wide memory free percentage: "', (err, stdout) => {
             if (err) {
                 deffered.reject(err);
             } else {
-                var data = stdout.replace('%','').replace(/[\s\n\r]+/g, ' ').split(' ');
+                let data = stdout.replace('%', '').replace(/[\s\n\r]+/g, ' ').split(' ');
                 deffered.resolve(data[4]);
             }
         });
-    //Linux platform
+        //Linux platform
     } else {
-        childProcess.exec('free -m', function(err, stdout) {
+        childProcess.exec('free -m', (err, stdout) => {
             if (err) {
                 deffered.reject(err);
             } else {
-                var data = stdout.split('\n')[1].replace(/[\s\n\r]+/g, ' ').split(' ');
-                var used = parseInt(data[2]);
-                var total = parseInt(data[1]);
+                let data = stdout.split('\n')[1].replace(/[\s\n\r]+/g, ' ').split(' ');
+                let used = parseInt(data[2]);
+                let total = parseInt(data[1]);
                 deffered.resolve(computeUsage(used, total));
             }
         });
@@ -73,15 +75,15 @@ exports.memoryUsage = function() {
     return deffered.promise;
 }
 
-exports.currentProcesses = function(sort) {
-    var deffered = q.defer();
-    var currentproc = [];
-    ps.get(function(err, processes) {
+exports.currentProcesses = sort => {
+    let deffered = q.defer();
+    let currentproc = [];
+    ps.get((err, processes) => {
         if (err) {
             deffered.reject(err);
         } else {
-            processes.forEach(function(proc) {
-                var process = {};
+            processes.forEach(proc => {
+                let process = {};
                 process.pid = proc.pid;
                 process.name = proc.name;
                 process.cpu = proc.cpu;
@@ -99,27 +101,27 @@ exports.currentProcesses = function(sort) {
     return deffered.promise;
 }
 
-exports.services = function(filters) {
-    var deffered = q.defer();
-    var listeServices = [];
+exports.services = filters => {
+    let deffered = q.defer();
+    let listeServices = [];
     if (process.platform === 'linux') {
-        childProcess.exec('service --status-all', function(err, stdout) {
+        childProcess.exec('service --status-all', (err, stdout) => {
             if (err) {
                 deffered.reject(err);
             } else {
-                var result = stdout.split('\n');
+                let result = stdout.split('\n');
                 result.splice(-1, 1);
-                result.forEach(function(line) {
-                    var data = line.split(']');
-                    var service = {};
+                result.forEach(line => {
+                    let data = line.split(']');
+                    let service = {};
                     service.name = data[1].trim();
                     service.runing = (data[0].trim().substring(2, 3) === '+') ? true : false;
                     listeServices.push(service);
                 });
 
                 if (filters) {
-                    var filteredServices = [];
-                    filters.forEach(function(filter) {
+                    let filteredServices = [];
+                    filters.forEach(filter => {
                         filteredServices.push(_.find(listeServices, filter));
                     });
                     deffered.resolve(filteredServices);
@@ -131,40 +133,41 @@ exports.services = function(filters) {
     } else {
         deffered.reject(new Error("Unsuported platform"));
     }
-
     return deffered.promise;
 }
 
 function getCPUInfo() {
-    var cpus = os.cpus();
+    let deffered = q.defer();
+    const cpus = os.cpus();
 
-    var user = 0;
-    var nice = 0;
-    var sys = 0;
-    var idle = 0;
-    var irq = 0;
-    var total = 0;
+    let user = 0,
+        nice = 0,
+        sys = 0,
+        idle = 0,
+        irq = 0,
+        total = 0;
 
-    for (var cpu in cpus) {
-
-        user += cpus[cpu].times.user;
-        nice += cpus[cpu].times.nice;
-        sys += cpus[cpu].times.sys;
-        irq += cpus[cpu].times.irq;
-        idle += cpus[cpu].times.idle;
-    }
-
-    var total = user + nice + sys + idle + irq;
-
-    return {
-        'idle': idle,
-        'total': total
-    };
+    q.forEach(cpus, cpu => {
+        let defer = q.defer();
+        user += cpu.times.user;
+        nice += cpu.times.nice;
+        sys += cpu.times.sys;
+        irq += cpu.times.irq;
+        idle += cpu.times.idle;
+        defer.resolve();
+        return defer.promise;
+    }).then(() => {
+        total = user + nice + sys + idle + irq;
+        deffered.resolve({
+            'idle': idle,
+            'total': total
+        })
+    });
+    return deffered.promise;
 }
 
 function winGetFreeMemory() {
-    var deffered = q.defer();
-
+    let deffered = q.defer();
     childProcess.exec('wmic os get freephysicalmemory /format:value', function(err, stdout) {
         if (err) {
             deffered.reject(err);
@@ -173,13 +176,11 @@ function winGetFreeMemory() {
             deffered.resolve(used);
         }
     });
-
     return deffered.promise;
 }
 
 function winGetTotalMemory() {
-    var deffered = q.defer();
-
+    let deffered = q.defer();
     childProcess.exec('wmic os get TotalVisibleMemorySize /format:value', function(err, stdout) {
         if (err) {
             deffered.reject(err);
@@ -188,6 +189,5 @@ function winGetTotalMemory() {
             deffered.resolve(used);
         }
     });
-
     return deffered.promise;
 }
